655644a29ab857c879e6d9b9a8dae18ec872b541
Michael Goodale <michaelericgoodale@gmail.com>
2016-12-04 20:23:52 -0500

    Added dropout probability params

--- clickbait_client.py
+++ clickbait_client.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from grpc.beta import implementations
 import tensorflow as tf
 import numpy as np
@@ -8,18 +9,18 @@ from tensorflow_serving.apis import predict_pb2
 from tensorflow_serving.apis import prediction_service_pb2
 
 
-tf.app.flags.DEFINE_string('server', 'localhost:9000',
-                           'PredictionService host:port')
-tf.app.flags.DEFINE_string('sentence', '', 'Sentence that is to be evaluated')
+tf.app.flags.DEFINE_string("server", "localhost:9000",
+                           "PredictionService host:port")
+tf.app.flags.DEFINE_string("sentence", "", "Sentence that is to be evaluated")
 FLAGS = tf.app.flags.FLAGS
 SENTENCE_MAX = 30
 WORD_VECTOR_SIZE = 50
 
 def main(_):
-    host, port = FLAGS.server.split(':')
+    host, port = FLAGS.server.split(":")
     channel = implementations.insecure_channel(host, int(port))
     stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
-    vectors_df = pd.read_csv('GloVeData/glove.6B.50d.txt',sep=' ',header=None,quoting=3)
+    vectors_df = pd.read_csv("GloVeData/glove.6B.50d.txt",sep=" ",header=None,quoting=3)
     vectors_df = vectors_df.set_index([0])
     EvalSentence = FLAGS.sentence
     EvalSentence = EvalSentence.lower().strip()
@@ -35,10 +36,11 @@ def main(_):
     data = sentence.flatten()
     data = data[np.newaxis,np.newaxis,:,np.newaxis]
     request = predict_pb2.PredictRequest()
-    request.model_spec.name = 'clickbait'
-    request.inputs['sentences'].CopyFrom(tf.contrib.util.make_tensor_proto(data, dtype = tf.float32, shape=[1,1,WORD_VECTOR_SIZE*SENTENCE_MAX,1]))
+    request.model_spec.name = "clickbait"
+    request.inputs["sentences"].CopyFrom(tf.contrib.util.make_tensor_proto(data, dtype = tf.float32))
+    request.inputs["dropout"].CopyFrom(tf.contrib.util.make_tensor_proto(np.ones([]), dtype=tf.float32))
     result = stub.Predict(request, 15.0)
     print(result)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     tf.app.run()
