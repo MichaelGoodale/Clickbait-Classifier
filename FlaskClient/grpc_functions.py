603ff3a9cbc34834e01b5ebf6ffe9b7dde107c8d
Michael Goodale <michaelericgoodale@gmail.com>
2016-12-07 17:01:17 -0500

    Removed clickbait_client and moved functionality to grpc_functions.py

--- FlaskClient/grpc_functions.py
+++ FlaskClient/grpc_functions.py
@@ -4,27 +4,17 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 import nltk
-
 from tensorflow_serving.apis import predict_pb2
 from tensorflow_serving.apis import prediction_service_pb2
 
-
-tf.app.flags.DEFINE_string("server", "localhost:9000",
-                           "PredictionService host:port")
-tf.app.flags.DEFINE_string("sentence", "", "Sentence that is to be evaluated")
-FLAGS = tf.app.flags.FLAGS
-SENTENCE_MAX = 30
-WORD_VECTOR_SIZE = 50
-
-def main(_):
-    host, port = FLAGS.server.split(":")
+def grpc_get(server, in_sentence, SENTENCE_MAX, WORD_VECTOR_SIZE):
+    host, port = server.split(":")
     channel = implementations.insecure_channel(host, int(port))
     stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
-    vectors_df = pd.read_csv("GloVeData/glove.6B.50d.txt",sep=" ",header=None,quoting=3)
+    vectors_df = pd.read_csv("../GloVeData/glove.6B.50d.txt",sep=" ",header=None,quoting=3)
     vectors_df = vectors_df.set_index([0])
-    EvalSentence = FLAGS.sentence
-    EvalSentence = EvalSentence.lower().strip()
-    wordArray = nltk.word_tokenize(EvalSentence)
+    sentence = in_sentence.lower().strip()
+    wordArray = nltk.word_tokenize(sentence)
     sentence = np.zeros((SENTENCE_MAX,WORD_VECTOR_SIZE))
     
     for i, word in enumerate(wordArray):
@@ -39,10 +29,7 @@ def main(_):
     request.model_spec.name = "clickbait"
     request.inputs["sentences"].CopyFrom(tf.contrib.util.make_tensor_proto(data, dtype = tf.float32))
     request.inputs["dropout"].CopyFrom(tf.contrib.util.make_tensor_proto(np.array(1), dtype=tf.float32))
-    result = stub.Predict(request, 15.0)
-    scores = tf.contrib.util.make_ndarray(result.outputs["scores"])
-    classes = tf.contrib.util.make_ndarray(result.outputs["classes"])
-    print("The sentence is "+str(round(scores[0,0]*100, 3))+"% "+str(classes[0]).lower()+" and "+str(round(scores[0,1]*100, 3))+"% "+str(classes[1])).lower()
-
-if __name__ == "__main__":
-    tf.app.run()
+    result = stub.Predict(request, 15.0)    
+    scores = tf.contrib.util.make_ndarray(result.outputs["scores"])[0].tolist() 
+    classes = tf.contrib.util.make_ndarray(result.outputs["classes"]).astype(str).tolist() 
+    return scores, classes
