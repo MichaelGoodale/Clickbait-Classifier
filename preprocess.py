912b86e0a4d4fd4de9dd9fb99f883d41731c8d6b
Michael Goodale <michaelericgoodale@gmail.com>
2016-12-06 23:16:06 -0500

    Added support for viral nova and reddit corpora to preprocess

--- preprocess.py
+++ preprocess.py
@@ -15,17 +15,22 @@ TEST_LABEL_FILE = DATA_PATH + "testLabels.txt"
 SENTENCE_MAX = 30
 WORD_VECTOR_SIZE = 50
 TRAIN_TEST_RATIO = 0.95
-files = [sys.argv[1], sys.argv[2]]
+files = [sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]]
 
 #Load files and remove any duplicate headlines
-clickbait_df = pd.read_csv(files[0])
+buzzfeed_df = pd.read_csv(files[0])
 news_df = pd.read_csv(files[1])
 news_df = news_df.drop_duplicates(["headline"])
-clickbait_df = clickbait_df.drop_duplicates(["headline"])
+buzzfeed_df = buzzfeed_df.drop_duplicates(["headline"])
+
+reddit_df = pd.read_csv(files[2], sep="\n")
+viral_df = pd.read_csv(files[3], sep="\n")
+reddit_df.columns = ["headline"]
+viral_df.columns = ["headline"]
 
 #Remove foreign languages, and news-ier titles
 ignored_cats = ["Espanol", "France", "Germany", "Brasil", "USNews", "World"]
-clickbait_df = clickbait_df.loc[~clickbait_df["article_category"].isin(ignored_cats)]
+buzzfeed_df = buzzfeed_df.loc[~buzzfeed_df["article_category"].isin(ignored_cats)]
 
 #load GloVe vectors
 print("Loading GloVe vectors")
@@ -50,6 +55,8 @@ def gloveify(df, headline_name, vectors):
     count = 0
     for i, row in df.iterrows():
         sentence = np.zeros((SENTENCE_MAX, WORD_VECTOR_SIZE))
+        if len(df.loc[i, headline_name]) > SENTENCE_MAX:
+            continue
         for j, word in enumerate(df.loc[i, headline_name]):
             if word in vectors.index:
                 sentence[j, :] = vectors.loc[word, :].values
@@ -60,16 +67,21 @@ def gloveify(df, headline_name, vectors):
     return(corpus)
 
 print("Cleaning text")
-clickbait_df = textCleaning(clickbait_df, "headline", 1)
+buzzfeed_df = textCleaning(buzzfeed_df, "headline", 1)
 news_df = textCleaning(news_df, "headline", 0)
+reddit_df = textCleaning(reddit_df, "headline", 0)
+viral_df = textCleaning(viral_df, "headline", 1)
 print("Text cleaned")
 
 print("Vectorising text")
-dataMat = gloveify(clickbait_df, "headline", vectors_df)
-dataMat = np.concatenate((dataMat, gloveify(news_df, "headline", vectors_df)), axis=0)
+buzzMat = gloveify(buzzfeed_df, "headline", vectors_df)
+newsMat = gloveify(news_df, "headline", vectors_df)
+redditMat = gloveify(reddit_df, "headline", vectors_df)
+viralMat = gloveify(viral_df, "headline", vectors_df)
+dataMat = np.concatenate((buzzMat, newsMat, redditMat, viralMat), axis=0)
 print("Text vectorised")
 
-labels = pd.concat([clickbait_df.loc[:, "ans"], news_df.loc[:, "ans"]])
+labels = pd.concat([buzzfeed_df.loc[:, "ans"], news_df.loc[:, "ans"], reddit_df.loc[:, "ans"], viral_df.loc[:, "ans"]])
 
 print("Shuffling and splitting")
 assert len(labels.as_matrix()) == len(dataMat)
