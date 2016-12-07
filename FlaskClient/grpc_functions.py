53181d140b2796ad42371de0efec0075c3b8e8d8
Michael Goodale <michaelericgoodale@gmail.com>
2016-12-07 17:04:00 -0500

    Moved embedding lookup to seperate function

--- FlaskClient/grpc_functions.py
+++ FlaskClient/grpc_functions.py
@@ -11,6 +11,17 @@ def grpc_get(server, in_sentence, SENTENCE_MAX, WORD_VECTOR_SIZE):
     host, port = server.split(":")
     channel = implementations.insecure_channel(host, int(port))
     stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
+    data = embedding_lookup(in_sentence)
+    request = predict_pb2.PredictRequest()
+    request.model_spec.name = "clickbait"
+    request.inputs["sentences"].CopyFrom(tf.contrib.util.make_tensor_proto(data, dtype = tf.float32))
+    request.inputs["dropout"].CopyFrom(tf.contrib.util.make_tensor_proto(np.array(1), dtype=tf.float32))
+    result = stub.Predict(request, 15.0)    
+    scores = tf.contrib.util.make_ndarray(result.outputs["scores"])[0].tolist() 
+    classes = tf.contrib.util.make_ndarray(result.outputs["classes"]).astype(str).tolist() 
+    return scores, classes
+    
+def embedding_lookup(in_sentence):
     vectors_df = pd.read_csv("../GloVeData/glove.6B.50d.txt",sep=" ",header=None,quoting=3)
     vectors_df = vectors_df.set_index([0])
     sentence = in_sentence.lower().strip()
@@ -24,12 +35,4 @@ def grpc_get(server, in_sentence, SENTENCE_MAX, WORD_VECTOR_SIZE):
             sentence[i,:]=np.ones(WORD_VECTOR_SIZE)
     
     data = sentence.flatten()
-    data = data[np.newaxis,np.newaxis,:,np.newaxis]
-    request = predict_pb2.PredictRequest()
-    request.model_spec.name = "clickbait"
-    request.inputs["sentences"].CopyFrom(tf.contrib.util.make_tensor_proto(data, dtype = tf.float32))
-    request.inputs["dropout"].CopyFrom(tf.contrib.util.make_tensor_proto(np.array(1), dtype=tf.float32))
-    result = stub.Predict(request, 15.0)    
-    scores = tf.contrib.util.make_ndarray(result.outputs["scores"])[0].tolist() 
-    classes = tf.contrib.util.make_ndarray(result.outputs["classes"]).astype(str).tolist() 
-    return scores, classes
+    return data[np.newaxis,np.newaxis,:,np.newaxis]
