9c3e1407fb88d21c0f7ffecd2ced1af70a6d83c3
Michael Goodale <michaelericgoodale@gmail.com>
2016-12-07 19:10:08 -0500

    Changed pandas lookup to SQL-Lite, for word embeddings

--- FlaskClient/grpc_functions.py
+++ FlaskClient/grpc_functions.py
@@ -2,16 +2,19 @@
 from grpc.beta import implementations
 import tensorflow as tf
 import numpy as np
-import pandas as pd
 import nltk
+from flask import g
+import sqlite3
 from tensorflow_serving.apis import predict_pb2
 from tensorflow_serving.apis import prediction_service_pb2
+
+DATABASE = 'database/word-vec.db'
 
 def grpc_get(server, in_sentence, SENTENCE_MAX, WORD_VECTOR_SIZE):
     host, port = server.split(":")
     channel = implementations.insecure_channel(host, int(port))
     stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
-    data = embedding_lookup(in_sentence)
+    data = sql_lookup(in_sentence, SENTENCE_MAX, WORD_VECTOR_SIZE)
     request = predict_pb2.PredictRequest()
     request.model_spec.name = "clickbait"
     request.inputs["sentences"].CopyFrom(tf.contrib.util.make_tensor_proto(data, dtype = tf.float32))
@@ -21,18 +24,25 @@ def grpc_get(server, in_sentence, SENTENCE_MAX, WORD_VECTOR_SIZE):
     classes = tf.contrib.util.make_ndarray(result.outputs["classes"]).astype(str).tolist() 
     return scores, classes
     
-def embedding_lookup(in_sentence):
-    vectors_df = pd.read_csv("../GloVeData/glove.6B.50d.txt",sep=" ",header=None,quoting=3)
-    vectors_df = vectors_df.set_index([0])
+def sql_lookup(in_sentence, SENTENCE_MAX, WORD_VECTOR_SIZE):
+    cur = get_db().cursor()
     sentence = in_sentence.lower().strip()
     wordArray = nltk.word_tokenize(sentence)
     sentence = np.zeros((SENTENCE_MAX,WORD_VECTOR_SIZE))
     
     for i, word in enumerate(wordArray):
-        if word in vectors_df.index:
-            sentence[i,:]=vectors_df.loc[word, :].values
+        word_string = (word,)
+        cur.execute("SELECT * FROM glove WHERE field1=?", word_string)
+        row = cur.fetchone()
+        if row is None:
+            sentence[i, :] = np.ones(WORD_VECTOR_SIZE)
         else:
-            sentence[i,:]=np.ones(WORD_VECTOR_SIZE)
-    
+            sentence[i] = np.array(row[1:]).astype(np.float32)
     data = sentence.flatten()
     return data[np.newaxis,np.newaxis,:,np.newaxis]
+    
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+       db = g._database = sqlite3.connect(DATABASE)
+    return db
